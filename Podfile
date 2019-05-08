fcb11de988dd73543e442a6becc1a084e6ad1319
boob <huangbo@yy.com>
2019-05-08 09:49:47 +0800

    rm

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform :osx, '10.11'
-
-target 'WeChatPlugin' do
-	pod 'JRSwizzle', '~> 1.0'
-    #pod "HockeySDK-Mac"
-end
--- Pods/Target Support Files/Pods-WeChatPlugin/Pods-WeChatPlugin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeChatPlugin : NSObject
-@end
-@implementation PodsDummy_Pods_WeChatPlugin
-@end
